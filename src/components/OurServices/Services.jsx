aee2d126beb7d6623a98b5bcc2c456a42e8fe274
Thyago Ribeiro <thyagoribeiro7226@gmail.com>
2024-11-10 11:46:39 -0300

    feat: services section

---
+++ src/components/OurServices/Services.jsx
@@ -0,0 +1,37 @@
+import { ServiceCard } from "../shared/Service-Card";
+import './Services.css';
+import ImagemCaixa from '../../assets/images/ImagemCaixa.avif';
+import ImagemDonut from '../../assets/images/ImagemDonut.avif';
+import ImagemMedalha from '../../assets/images/ImagemMedalha.avif';
+export function ServicesSection () {
+    return (
+        <section className="services-section container">
+            <div className="services-cards">
+                <ServiceCard 
+                image={ImagemCaixa} 
+                altText={'Imagem de Uma Caixa 3d Com um Relógio Simbolizando o Rápido Serviço'}
+                title={'Rápido Delivery!'}
+                description={
+                    'Nosso dever é demonstrar uma incrível experiência até em nosso delivery!'
+                }
+                />
+                <ServiceCard 
+                image={ImagemDonut} 
+                altText={'Imagem de um Donut 3d Simbolizando a Qualidade dos Doces'}
+                title={'Doces Fresquinhos!'}
+                description={
+                    'Nossos doces são sempre preparados com o maior carinho!'
+                }
+                />
+                <ServiceCard 
+                image={ImagemMedalha}
+                altText={'Imagem de uma medalha 3d Simbolizando a Qualidade da Loja'}
+                title={'Doces Fresquinhos!'}
+                description={
+                    'Somos Reconhecidos pelos maiores chefes especializados em confeitaria!'
+                }
+                />
+            </div>
+        </section>
+    );
+} 
