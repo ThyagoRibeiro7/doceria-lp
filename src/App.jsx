edca160aeb25acedfeeb4c23a7ea2a440d30f95d
Thyago Ribeiro <thyagoribeiro7226@gmail.com>
2024-11-10 13:05:32 -0300

    fixing some imports errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { NavBar } from "./components/navbar/navBar.jsx";
-import { Hero } from "./components/herosection/heroContent.jsx";
+import { Hero } from "./components/heroSection/heroContent.jsx";
 import { ServicesSection } from "./components/ourservices/services.jsx";
 import { HighLights } from "./components/highLights/highLights.jsx";
 import { ChefHistory } from "./components/chefshistory/chefHistory.jsx";
